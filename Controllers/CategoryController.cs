c03f740573ceb55688d66f10832b7f0a7c15ca07
Tran Quoc Bao <baotq.2901@gmail.com>
2024-11-22 10:59:49 +0700

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyWebApplication.Data;
+using MyWebApplication.Models;
+
+namespace MyWebApplication.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            // list all the category/ using Dependency Injection
+            List<Category> categoryList = _db.Categories.ToList();
+            return View(categoryList);
+        }
+        // 1: Create a category
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost] // handler the function
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot be the same exactly with the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // 2: Edit a category (update)
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) // check if it is not a valid id
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+             if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost] // handler the function
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // 3: Delete a category
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0) // check if it is not a valid id
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")] // handler the function
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebApplication.Models;
+
+namespace MyWebApplication.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            // adding configurations : whats options will be passed on DBContext
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //default entity framework
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Play", DisplayOrder = 1 },
+                new Category { CategoryId = 2, Name = "SciFi", DisplayOrder = 5 },
+                new Category { CategoryId = 3, Name = "Romantic", DisplayOrder = 6 }
+            );
+        }
+    }
+}
